3bcac8ce9201c9b0d770a0530db0305d0b732adb
kuhree <git@kuhree.com>
2025-04-22 23:31:05 -0400

    chore: move settings back to nix

--- flake.nix
+++ flake.nix
@@ -1,46 +1,9 @@
 {
   description = "Kuhree's NixOS Config(s)";
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org"
-      "https://cache.garnix.io/"
-      "https://nix-community.cachix.org/"
-      "https://hyprland.cachix.org/"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-    experimental-features = [
-      # "ca-derivations"
-      # "recursive-nix"
-      "flakes"
-      "nix-command"
-    ];
-
-    accept-flake-config = true;
-    auto-optimise-store = true;
-    builders-use-substitutes = true; # use binary cache, its not gentoo
-    commit-lock-file-summary = "chore: Update flake.lock";
-    flake-registry = "/etc/nix/registry.json";
-    http-connections = 50;
-    show-trace = true;
-    trusted-users = ["@wheel" "@builders"];
-    warn-dirty = false;
-  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
-
-    # Unused flakes
-    # ghostty.url = "github:ghostty-org/ghostty";
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    #   # inputs.nixpkgs.follows = "nixpkgs"; # If mesa/opengl gets a mismatch
-    # };
   };
 
   outputs = {...} @ inputs: let
--- modules/nix.nix
+++ modules/nix.nix
@@ -29,6 +29,32 @@ in {
       # Optimize build performance
       daemonCPUSchedPolicy = "batch";
       daemonIOSchedClass = "best-effort";
+
+      settings = {
+        extra-substituters = [
+          "https://cache.nixos.org"
+          "https://cache.garnix.io/"
+          "https://nix-community.cachix.org/"
+          "https://hyprland.cachix.org/"
+        ];
+        extra-trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        ];
+
+        accept-flake-config = true;
+        auto-optimise-store = true;
+        builders-use-substitutes = true; # use binary cache, its not gentoo
+        commit-lock-file-summary = "chore: Update flake.lock";
+        experimental-features = [ "flakes" "nix-command" ];
+        flake-registry = "/etc/nix/registry.json";
+        http-connections = 50;
+        show-trace = true;
+        trusted-users = ["@wheel" "@builders"];
+        warn-dirty = false;
+      };
     };
 
     systemd = {
